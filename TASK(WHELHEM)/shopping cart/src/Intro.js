412d1bb34c50c0eb7a7051db0d2e43762e75e833
KavyaJegadesan <148220452+KavyaJegadesan@users.noreply.github.com>
2023-12-30 17:39:47 +0530

    Delete TASK(WHELHEM)/shopping cart directory

--- TASK(WHELHEM)/shopping cart/src/Intro.js
+++
@@ -1,8 +0,0 @@
-export function Intro() {
-  return (
-    <div id="intro-div">
-      <p id="intro-title">Shop in Style <br></br> <span id="intro-span">With this shop homepage template</span></p>
-      <p></p>
-    </div>
-  );
-}
